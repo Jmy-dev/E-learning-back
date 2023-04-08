28c63797b311e6bbff4d5ec8bd037ba87747f792
Jmy-dev <ay9122000@gmail.com>
2023-04-09 00:36:56 +0200

    i hope it's the last oneeee

--- dist/config/dev.js
+++ dist/config/dev.js
@@ -6,10 +6,11 @@ Object.defineProperty(exports, "__esModule", {
 exports.config = void 0;
 var config = {
   secrets: {
-    dbConnection: 'mongodb+srv://e-learning.fkai0h7.mongodb.net/E-learning-app',
+    dbConnection: 'mongodb+srv://user:E2bqyIuMOAllffPX@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority',
     jwtSecret: 'WinterIsComing',
     jwtExp: '1d'
   }
 };
 //127.0.0.1
+//mongodb+srv://admin:<password>@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority
 exports.config = config;
