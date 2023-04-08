76268a0a87dae42bf0c1dc79919893f32bbab9dd
Jmy-dev <61315548+Jmy-dev@users.noreply.github.com>
2023-04-09 00:45:55 +0200

    Delete dev.js

--- app/config/dev.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-    secrets: {
-        dbConnection: 'mongodb+srv://user:E2bqyIuMOAllffPX@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority',
-        jwtSecret: 'WinterIsComing',
-        jwtExp: '1d'
-    }
-}
-//127.0.0.1
-//mongodb+srv://admin:<password>@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority
