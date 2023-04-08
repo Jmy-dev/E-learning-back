383cfc9384cc92e89de2f053812f9bbbd88c6b91
Jmy-dev <ay9122000@gmail.com>
2023-04-09 00:24:50 +0200

    i hope it's the last one

--- app/config/dev.js
+++ app/config/dev.js
@@ -1,6 +1,6 @@
 export const config = {
     secrets: {
-        dbConnection: 'mongodb+srv://admin:HW1g12vpcrtMeLJD@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority',
+        dbConnection: 'mongodb+srv://user:E2bqyIuMOAllffPX@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority',
         jwtSecret: 'WinterIsComing',
         jwtExp: '1d'
     }
