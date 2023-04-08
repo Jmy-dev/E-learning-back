7dcc5358250edf356ba8e5ec1ff945c57873ae5e
Jmy-dev <ay9122000@gmail.com>
2023-04-09 00:07:13 +0200

    last update of connection string

--- app/config/dev.js
+++ app/config/dev.js
@@ -1,8 +1,9 @@
 export const config = {
     secrets: {
-        dbConnection: 'mongodb+srv://<admin>:<Valo1234!@#$>@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority',
+        dbConnection: 'mongodb+srv://admin:HW1g12vpcrtMeLJD@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority',
         jwtSecret: 'WinterIsComing',
         jwtExp: '1d'
     }
 }
 //127.0.0.1
+//mongodb+srv://admin:<password>@e-learning.fkai0h7.mongodb.net/?retryWrites=true&w=majority
